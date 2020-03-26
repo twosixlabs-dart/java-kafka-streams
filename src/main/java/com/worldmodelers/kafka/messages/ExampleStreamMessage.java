f423c361ca8d3665da271005c54436afbcb43af0
John Hungerford <john.hungerford@labsadminsMBP2.fios-router.home>
2020-03-25 21:34:57 -0400

    made ExampleStreamsMessage a java bean

--- src/main/java/com/worldmodelers/kafka/messages/ExampleStreamMessage.java
+++ src/main/java/com/worldmodelers/kafka/messages/ExampleStreamMessage.java
@@ -4,28 +4,49 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class ExampleStreamMessage {
+    private static long serialVersionUID = 2L;
+
     @JsonProperty( "id" )
     private String id;
 
     @JsonProperty( "breadcrumbs" )
-    private ArrayList<String> breadcrumbs = new ArrayList<>();
+    private List<String> breadcrumbs;
+
 
     @JsonCreator
-    public ExampleStreamMessage( @JsonProperty( "id" ) String idIn, @JsonProperty( "breadcrumbs" ) ArrayList<String> breadcrumbsIn ) {
+    public ExampleStreamMessage( @JsonProperty( "id" ) String id, @JsonProperty( "breadcrumbs" ) List<String> breadcrumbs ) {
+        this.id = id;
+        if ( breadcrumbs == null ) {
+            this.breadcrumbs = new ArrayList<>();
+        } else {
+            this.breadcrumbs = breadcrumbs;
+        }
+    }
+
+    public ExampleStreamMessage() {
+        this.id = null;
+        this.breadcrumbs = null;
+    }
+
+    public void setId( String idIn ) {
         id = idIn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setBreadcrumbs( ArrayList<String> breadcrumbsIn ) {
         breadcrumbs = breadcrumbsIn;
     }
 
-    public void setId( String idIn ) { id = idIn; }
-
-    public String getId() { return id; }
-
-    public void setBreadcrumbs( ArrayList<String> breadcrumbsIn ) { breadcrumbs = breadcrumbsIn; }
-
-    public ArrayList<String> getBreadcrumbs() { return breadcrumbs; }
+    public List<String> getBreadcrumbs() {
+        return breadcrumbs;
+    }
 
     @Override
     public boolean equals( Object o ) {
