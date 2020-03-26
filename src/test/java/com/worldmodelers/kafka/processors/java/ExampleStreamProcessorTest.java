d15847c63c39047fc4cda9223d2b99d62ec93e3e
John Hungerford <john.hungerford@labsadminsMBP2.fios-router.home>
2020-03-25 21:42:23 -0400

    cleaned up test

--- src/test/java/com/worldmodelers/kafka/processors/java/ExampleStreamProcessorTest.java
+++ src/test/java/com/worldmodelers/kafka/processors/java/ExampleStreamProcessorTest.java
@@ -7,8 +7,6 @@ import kafka.server.KafkaConfig$;
 import net.mguenther.kafka.junit.*;
 import org.junit.Rule;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,11 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ExampleStreamProcessorTest extends ExampleStreamMessageJsonFormat {
-
-    private final Logger LOG = LoggerFactory.getLogger( ExampleStreamProcessor.class );
 
     private EmbeddedKafkaConfig kafkaConfig = EmbeddedKafkaConfig
             .create()
